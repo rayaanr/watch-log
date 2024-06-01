66b87440ea8ed19c05605451d5a37517354627a9
Ꮢαყααɳ <rayaannsn@gmail.com>
2024-06-01 23:56:06 +0530

    cleanup

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Header from "@/components/header";
-
-export default function Home() {
-    return (
-        <div className="flex min-h-screen w-full flex-col">
-            <Header />
-        </div>
-    );
-}
--- src/app/providers.tsx
+++
@@ -1,9 +0,0 @@
-import { ThemeProvider } from "@/context/theme-provider";
-
-export default function Providers({ children }: { children: React.ReactNode }) {
-    return (
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-            {children}
-        </ThemeProvider>
-    );
-}
